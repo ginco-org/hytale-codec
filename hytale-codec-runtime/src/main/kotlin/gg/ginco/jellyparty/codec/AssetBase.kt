650909054942c90b66fc3dca6f8d4876073684a8
taya <taya@taya.net>
2026-03-06 16:49:25 +0100

    fix: make _id public so generated codecs in consuming modules can access it

--- hytale-codec-runtime/src/main/kotlin/gg/ginco/jellyparty/codec/AssetBase.kt
+++ hytale-codec-runtime/src/main/kotlin/gg/ginco/jellyparty/codec/AssetBase.kt
@@ -6,6 +6,6 @@ import com.hypixel.hytale.assetstore.map.JsonAssetWithMap
 
 abstract class AssetBase<T : AssetBase<T>> : JsonAssetWithMap<String, DefaultAssetMap<String, T>> {
     var data: AssetExtraInfo.Data? = null
-    internal var _id: String? = null
+    var _id: String? = null
     override fun getId(): String = _id ?: throw IllegalStateException("${this::class.simpleName} ID not set")
 }
